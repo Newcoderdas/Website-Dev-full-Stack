1d896e970616eb37aad12032f57ac1273ebf2579
Rizwan Hussain <hrizwan502@gmail.com>
2024-03-13 01:39:50 +0500

    sdsadas
    qwsaedasd

--- project1/script.js
+++ project1/script.js
@@ -1,14 +1,23 @@
-//elements retrieve karwa rhy
-const form = document.getElementById('form');
 const UserName = document.getElementById('username');
 const Email = document.getElementById('Email');
 const Password = document.getElementById('Password');
 const ConfirmPassword = document.getElementById('ConfirmPassword');
+const btn = document.getElementById('btn');
+
+const form = document.getElementById('form');
 
 form.addEventListener('submit', function(e) {
-    //stoping page from refresh
     e.preventDefault();
-    console.log('SUBMITTED');
-
-
+    console.log(UserName.value);
+    console.log(Email.value);
+    console.log(Password.value);
+    console.log(ConfirmPassword.value);
+    validation();
 });
+
+function validation() {
+    if (!UserName.value) {
+        console.log('null value');
+
+    }
+}
